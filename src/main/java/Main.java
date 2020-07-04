b98b388edec3da2e117edd11ccb6a59abdd3bb36
Ewa <strojna.ew@gmail.com>
2020-07-04 12:41:15 +0200

    happiness is a journey - establish connection with api

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Main {
+    public static void main(String[] args) {
+
+        try {
+            URL url = new URL("https://api.adviceslip.com/advice");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuffer content = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+            System.out.println(content.toString());
+            in.close();
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
 }
